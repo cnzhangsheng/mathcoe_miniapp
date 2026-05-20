996769e8eef5b0946b940efb097cf0c7053bd6e0
cnshzs <cnzhangsheng@outlook.com>
2026-05-20 15:58:31 +0800

    更新代码 2026-05-20 15:58

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ App({
   globalData: {
     userInfo: null,
     token: null,
-    baseUrl: 'http://127.0.0.1:8000/api/v1',  // 开发环境（本地调试）
-    // baseUrl: 'https://aicoe.cn/api/v1',  // 生产环境（线上部署）
+    // baseUrl: 'http://127.0.0.1:8000/api/v1',  // 开发环境（本地调试）
+    baseUrl: 'https://aicoe.cn/api/v1',  // 生产环境（线上部署）
     isLoggedIn: false
   },
 
