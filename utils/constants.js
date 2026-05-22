1f293e32e9fe0dc0de47248dac0db23a12022db6
cnshzs <cnzhangsheng@outlook.com>
2026-05-22 23:01:35 +0800

    f

--- utils/constants.js
+++ utils/constants.js
@@ -16,7 +16,7 @@ const CONTENT_BASE_URL = BASE_URL
 const DIFFICULTY_LEVELS = [
   { value: 1, label: 'Level 1' },
   { value: 2, label: 'Level 2' },
-  // { value: 3, label: 'Level 3' },
+  { value: 3, label: 'Level 3' },
   // { value: 4, label: 'Level 4' },
   // { value: 5, label: 'Level 5' },
   // { value: 6, label: 'Level 6' },
